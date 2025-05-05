3b4b3b347327870a930826cd6081ef265f80a116
Steve Roach <sroachst@gmail.com>
2025-05-05 08:35:51 -0400

    Add application entry point with Spring Boot setup
    Introduce the main application class with Spring Boot annotations, including scheduling support. This forms the foundation for starting and running the application.

---
+++ src/main/kotlin/gy/roach/monitor/service/App.kt
@@ -0,0 +1,15 @@
+package gy.roach.monitor.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@SpringBootApplication
+@EnableScheduling
+class App {
+}
+
+fun main(args: Array<String>) {
+    runApplication<App>(*args)
+
+}
